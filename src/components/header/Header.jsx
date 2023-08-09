214ff59312814fed6b94d6aa59b195dc621e49bf
ShadePrima <aleksandrag1984@gmail.com>
2023-08-09 16:22:03 +0300

    header main

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -6,6 +6,16 @@ const Header = () => {
   return (
     <div className={styles.root}>
       <Navigation />
+      <div className={styles.content}>
+        <h2 className={styles.firstLine}>FREE 15-DAY TRIAL</h2>
+        <h1 className={styles.secondLine}>
+          Change Your Energy <br /> Transform Your Life
+        </h1>
+        <h2 className={styles.thirdLine}>
+          Learn from mind-body-spirit masters on a premium online platform
+        </h2>
+        <button className={styles.button}>Start Free-15 Day Trial</button>
+      </div>
     </div>
   )
 }
