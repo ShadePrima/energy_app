d0e1b24bc8985d94541ae18418c968d6102aed5a
ShadePrima <aleksandrag1984@gmail.com>
2023-08-09 13:21:42 +0300

    mobile navigation

--- src/components/navigation/Navigation.jsx
+++ src/components/navigation/Navigation.jsx
@@ -4,6 +4,7 @@ import logo from '../../assets/icons/logo.svg'
 import search from '../../assets/icons/search.svg'
 import cart from '../../assets/icons/cart-bg.png'
 import persone from '../../assets/icons/persone.svg'
+import burger from '../../assets/icons/burger-menu.svg'
 
 import styles from './Navigation.module.scss'
 
@@ -22,9 +23,10 @@ const Navigation = () => {
       </div>
 
       <div className={styles.iconeMenu}>
-        <img src={search} alt='Search' />
-        <img src={persone} alt='Persone' />
+        <img src={search} className={styles.search} alt='Search' />
+        <img src={persone} className={styles.persone} alt='Persone' />
         <img src={cart} className={styles.cart} alt='Cart' />
+        <img src={burger} className={styles.burger} alt='Burger' />
       </div>
     </div>
   )
