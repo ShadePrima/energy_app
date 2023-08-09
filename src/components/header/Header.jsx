4e9604ff4462a0721c8ec7ba6e7aa6284e30dd36
ShadePrima <aleksandrag1984@gmail.com>
2023-08-09 16:55:36 +0300

    header mobile

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,5 +1,8 @@
 import Navigation from '../navigation/Navigation'
 
+import scroll from '../../assets/icons/scroll.svg'
+import girl from '../../assets/images/girl.png'
+
 import styles from './Header.module.scss'
 
 const Header = () => {
@@ -9,12 +12,14 @@ const Header = () => {
       <div className={styles.content}>
         <h2 className={styles.firstLine}>FREE 15-DAY TRIAL</h2>
         <h1 className={styles.secondLine}>
-          Change Your Energy <br /> Transform Your Life
+          Change Your Energy Transform Your Life
         </h1>
         <h2 className={styles.thirdLine}>
           Learn from mind-body-spirit masters on a premium online platform
         </h2>
         <button className={styles.button}>Start Free-15 Day Trial</button>
+        <img className={styles.scroll} src={scroll} alt='Scroll' />
+        <img className={styles.girl} src={girl} alt='Girl' />
       </div>
     </div>
   )
